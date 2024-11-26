f19f431d497c46870d1f206eac779517733572d0
zpzporange <pan-zeng@outlook.com>
2024-11-27 00:06:01 +0800

    next

--- network/organizations/ccp-generate.sh
+++ network/organizations/ccp-generate.sh
@@ -30,38 +30,38 @@ function yaml_ccp {
 ORG=MiningCompanyMSP
 P0PORT=8051
 CAPORT=8054
-PEERPEM=../organizations/peerOrganizations/miningcompany.example.com/tlsca/tlsca.miningcompany.example.com-cert.pem
-CAPEM=../organizations/peerOrganizations/miningcompany.example.com/ca/ca.miningcompany.example.com-cert.pem
+PEERPEM=organizations/peerOrganizations/miningcompany.example.com/tlsca/tlsca.miningcompany.example.com-cert.pem
+CAPEM=organizations/peerOrganizations/miningcompany.example.com/ca/ca.miningcompany.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../organizations/peerOrganizations/miningcompany.example.com/connection-miningcompany.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../organizations/peerOrganizations/miningcompany.example.com/connection-miningcompany.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/miningcompany.example.com/connection-miningcompany.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/miningcompany.example.com/connection-miningcompany.yaml
 
 # CuttingCompanyMSP
 ORG=CuttingCompanyMSP
 P0PORT=9051
 CAPORT=9054
-PEERPEM=../organizations/peerOrganizations/cuttingcompany.example.com/tlsca/tlsca.cuttingcompany.example.com-cert.pem
-CAPEM=../organizations/peerOrganizations/cuttingcompany.example.com/ca/ca.cuttingcompany.example.com-cert.pem
+PEERPEM=organizations/peerOrganizations/cuttingcompany.example.com/tlsca/tlsca.cuttingcompany.example.com-cert.pem
+CAPEM=organizations/peerOrganizations/cuttingcompany.example.com/ca/ca.cuttingcompany.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../organizations/peerOrganizations/cuttingcompany.example.com/connection-cuttingcompany.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../organizations/peerOrganizations/cuttingcompany.example.com/connection-cuttingcompany.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/cuttingcompany.example.com/connection-cuttingcompany.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/cuttingcompany.example.com/connection-cuttingcompany.yaml
 
 # GradingLabMSP
 ORG=GradingLabMSP
 P0PORT=10051
 CAPORT=10054
-PEERPEM=../organizations/peerOrganizations/gradinglab.example.com/tlsca/tlsca.gradinglab.example.com-cert.pem
-CAPEM=../organizations/peerOrganizations/gradinglab.example.com/ca/ca.gradinglab.example.com-cert.pem
+PEERPEM=organizations/peerOrganizations/gradinglab.example.com/tlsca/tlsca.gradinglab.example.com-cert.pem
+CAPEM=organizations/peerOrganizations/gradinglab.example.com/ca/ca.gradinglab.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../organizations/peerOrganizations/gradinglab.example.com/connection-gradinglab.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../organizations/peerOrganizations/gradinglab.example.com/connection-gradinglab.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/gradinglab.example.com/connection-gradinglab.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/gradinglab.example.com/connection-gradinglab.yaml
 
 # JewelryMakerMSP
 ORG=JewelryMakerMSP
 P0PORT=11051
 CAPORT=11054
-PEERPEM=../organizations/peerOrganizations/jewelrymaker.example.com/tlsca/tlsca.jewelrymaker.example.com-cert.pem
-CAPEM=../organizations/peerOrganizations/jewelrymaker.example.com/ca/ca.jewelrymaker.example.com-cert.pem
+PEERPEM=organizations/peerOrganizations/jewelrymaker.example.com/tlsca/tlsca.jewelrymaker.example.com-cert.pem
+CAPEM=organizations/peerOrganizations/jewelrymaker.example.com/ca/ca.jewelrymaker.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../organizations/peerOrganizations/jewelrymaker.example.com/connection-jewelrymaker.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > ../organizations/peerOrganizations/jewelrymaker.example.com/connection-jewelrymaker.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/jewelrymaker.example.com/connection-jewelrymaker.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrganizations/jewelrymaker.example.com/connection-jewelrymaker.yaml
